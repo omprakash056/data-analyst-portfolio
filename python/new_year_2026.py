c7e57dbe0773a1d8d2e16570862b105544d4d37d
OMPRAKASH <oom880240@gmail.com>
2025-12-31 18:29:00 +0530

    New Year 2026 greeting program added

---
+++ python/new_year_2026.py
@@ -0,0 +1,81 @@
+print("                           ")
+print("Welcome in 2026 💐🎆🎉😀 ")
+
+name = (input("\n Inter your name please"))
+print ("Happy new year",name)
+print ('''Choose your relathion
+       \n brother. sister. mother. father. friend. teacher. relative''')
+choise = input("Inter your choise (brother/sister/mother/father/friends/teacher/relatives)").lower()
+
+if choise == "brother":
+    print("\n  💐 This is for you bro..")
+    print('''As the New Year unfolds,I wish,
+     you a year fillled with love,
+        happiness and togetherness🎆🎉 ''') 
+    print('''                                           
+                                            Thank you
+                                              date:-
+                                            01-01-2026''')
+    
+elif choise == "sister":
+    print("\n  💐 This is for you sis..")
+    print('''As the New Year unfolds,I wish,
+     you a year fillled with love,
+        happiness and togetherness🎆🎉 ''') 
+    print('''                                           
+                                            Thank you
+                                              date:-
+                                            01-01-2026''')
+    
+elif choise == "mother":
+    print("\n  💐 This is for you mom..")
+    print('''As the New Year unfolds,I wish,
+     you a year fillled with love,
+        happiness and togetherness🎆🎉 ''') 
+    print('''                                           
+                                            Thank you
+                                              date:-
+                                            01-01-2026''')
+
+elif choise == "father":
+    print("\n  💐 This is for you dad..")
+    print('''As the New Year unfolds,I wish,
+     you a year fillled with love,
+        happiness and togetherness🎆🎉 ''') 
+    print('''                                           
+                                            Thank you
+                                              date:-
+                                            01-01-2026''')
+
+elif choise == "friend":
+    print("\n  💐 This is for you dear..")
+    print('''As the New Year unfolds,I wish,
+     you a year fillled with love,
+        happiness and togetherness🎆🎉 ''') 
+    print('''                                           
+                                            Thank you
+                                              date:-
+                                            01-01-2026''')
+    
+elif choise == "teacher":
+    print("\n 💐 This is for you dear sir.")
+    print('''As the New Year unfolds,I wish,
+     you a year fillled with love,
+        happiness and togetherness🎆🎉 ''')
+    print('''                                           
+                                            Thank you
+                                              date:-
+                                            01-01-2026''')
+    
+elif choise == "relative":
+    print("\n  💐 This is for you dear..")
+    print('''As the New Year unfolds,I wish,
+     you a year fillled with love,
+        happiness and togetherness🎆🎉 ''')
+    print('''                                           
+                                            Thank you
+                                              date:-
+                                            01-01-2026''')
+
+else:
+    print("\n❌ Invalid choice") 
