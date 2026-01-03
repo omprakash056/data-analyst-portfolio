49444ccedc8170b51724a233a169a1dc65955d97
OMPRAKASH <oom880240@gmail.com>
2026-01-03 15:32:45 +0530

    Added filtering sorting and aggregate SQL queries
    Added additional SQL queries for filtering, sorting, and aggregate functions.

--- sql/basic_queries.sql
+++ sql/basic_queries.sql
@@ -1,7 +1,28 @@
-__Basic SQL Queries Practice
+--Basic SQL Queries Practice
   
 SELECT * FROM employees;
 
 SELECT name, salary FROM employees WHERE salary > 30000;
 
 SELECT department, COUNT(*) FROM employees GROUP BY department;
+
+
+
+--Filtering and sorting 
+
+SELECT * FROM employees
+WHERE department = 'Sales';
+
+SELECT name, salary
+FROM employees
+ORDER BY salary DESC;
+
+-- Aggregate Functions
+
+SELECT department, AVG(salary) AS avg_salary
+FROM employees
+GROUP BY department;
+
+SELECT department, COUNT(*) AS total_employees
+FROM employees
+GROUP BY department; 
